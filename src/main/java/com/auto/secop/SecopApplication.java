f1986b7db9b96de02b61ee9dccb4e26e2ca2d620
Camila Castro <ortiz@192.168.1.12>
2026-09-20 14:51:04 -0500

    new proyect

---
+++ src/main/java/com/auto/secop/SecopApplication.java
@@ -0,0 +1,13 @@
+package com.auto.secop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/secop/SecopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.secop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
